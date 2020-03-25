e3ca361a63cc4cadb9ab0709cc6bc584f6914409
alexbarksdale <ale.barksdale@gmail.com>
2020-03-25 03:55:29 -0500

    [Starter project]

---
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
+    }
+});
+
+const ComponentsScreen = () => {
+    const greeting = 'Hi there';
+
+    return (
+        <View>
+            <Text style={styles.textStyle}>This is the components screen</Text>
+            <Text style={styles.textStyle}>{greeting}</Text>
+        </View>
+    );
+};
+
+export default ComponentsScreen;
+
